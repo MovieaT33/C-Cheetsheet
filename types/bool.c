984d5dca9d04641a6e62350df132b53d54336ec9
Movie <github.drinking886@passinbox.com>
2026-05-27 22:23:54 +0300

    Update bool.c

--- types/bool.c
+++ types/bool.c
@@ -1,17 +1,19 @@
 #include <stdbool.h>
 
-// #define bool _Bool
+// #define bool _Bool // C99
 // #define true 1
 // #define false 0
 
-int main(void) {
+int main(void)
+{
+    // sizeof(bool) = 1
     bool boolean_false = false;
     bool boolean_true  = true;
 
     bool a = 45;
-    a == true;   // Evaluates to true (non-zero)
+    !!a == true; // evaluates to true (non-zero)
     bool b = 0;
-    b == false;  // Evaluates to false (zero)
+    !!b == false; // evaluates to false (zero)
 
     return false;
 }
