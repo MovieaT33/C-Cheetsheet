b6919cec8cd2f80cf512fa35a3b51f11674a0309
Movie <github.drinking886@passinbox.com>
2026-06-05 21:07:06 +0300

    Update gcc.c

--- functions/gcc.c
+++ functions/gcc.c
@@ -1,3 +1,12 @@
 /* See all GCC functions attributes here:
 https://gcc.gnu.org/onlinedocs/gcc-3.4.6/gcc/Function-Attributes.html#Function-Attributes
 */
+
+#include <stdlib.h>
+
+void bar(void* ptr) {}
+
+void foo(void)
+{
+    __attribute__((cleanup(bar)) char *buf = malloc(1'024);
+}
